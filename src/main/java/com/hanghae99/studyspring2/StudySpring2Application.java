471a18b15d5038c1b789f633bd4ccb332e82f1e5
dongil618 <rlaehddlf618@gmail.com>
2022-07-01 10:27:30 +0900

    CREATE Spring Project

---
+++ src/main/java/com/hanghae99/studyspring2/StudySpring2Application.java
@@ -0,0 +1,13 @@
+package com.hanghae99.studyspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpring2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae99/studyspring2/StudySpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.studyspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
