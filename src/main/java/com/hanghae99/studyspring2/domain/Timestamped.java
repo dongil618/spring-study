5391ee50bc138a579484f6aebeeb42443422e433
dongil618 <rlaehddlf618@gmail.com>
2022-07-05 22:02:40 +0900

    UPDATE Timestamped.java - Getter어노테이션 추가, Getter가 없으면 생성날짜, 수정날짜를 못 받아옴.

--- src/main/java/com/hanghae99/studyspring2/domain/Timestamped.java
+++ src/main/java/com/hanghae99/studyspring2/domain/Timestamped.java
@@ -1,5 +1,6 @@
 package com.hanghae99.studyspring2.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 
 @MappedSuperclass // 상속했을 때, 컬럼으로 인식하게 합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/수정 시간을 자동으로 반영하도록 설정
+@Getter // Getter가 없으면 작동이 안됨.
 public abstract class Timestamped {             // abstract는 상속이 되어야지만 사용할 수 있는 클래스라는 것을 의미
 
     @CreatedDate // 생성일자임을 나타냅니다.
