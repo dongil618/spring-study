416aaec0885ea90239a2339425ac232939b5fc3d
dongil618 <rlaehddlf618@gmail.com>
2022-07-05 22:03:56 +0900

    UPDATE Post.java - 포스팅 제목, 비밀번호 추가

--- src/main/java/com/hanghae99/studyspring2/domain/Post.java
+++ src/main/java/com/hanghae99/studyspring2/domain/Post.java
@@ -2,11 +2,13 @@ package com.hanghae99.studyspring2.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Getter
 @Entity
+@ToString
 @NoArgsConstructor
 public class Post extends Timestamped{
 
@@ -18,15 +20,25 @@ public class Post extends Timestamped{
     private String username;
 
     @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
     private String contents;
 
-    public Post(String username, String contents){
+    public Post(String username, String title, String password, String contents){
         this.username = username;
+        this.title = title;
+        this.password = password;
         this.contents = contents;
     }
 
     public Post(PostRequestDto requestDto){
         this.username = requestDto.getUsername();
+        this.title = requestDto.getTitle();
+        this.password = requestDto.getPassword();
         this.contents = requestDto.getContents();
     }
 
